44d537000d3ef10a600712b05c7698b32226544d
prima101112 <prima101112@gmail.com>
2018-09-07 11:19:15 +0700

    handler exec comand pull

--- handler.go
+++ handler.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"time"
 )
 
 var BRANCH string
+var PATH string
 
 func init() {
 	branchPtr := flag.String("branch", "master", "a string")
+	pathPtr := flag.String("path", "/home/go/src/github.com/repo", "a string")
 	flag.Parse()
 	BRANCH = *branchPtr
+	PATH = *pathPtr
+
+	_, err := exec.LookPath("git")
+	if err != nil {
+		log.Fatal("git not found")
+	}
 }
 
 func GithubHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,7 +45,25 @@ func GithubHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if res["ref"] == "refs/heads/"+BRANCH {
-		log.Print("this is master doing something")
+		pull()
+		log.Println("get push ok")
+		http.Error(w, "ok", 200)
+		return
 	}
+}
 
+func pull() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "git", "pull", "--no-ff", "origin", BRANCH)
+	cmd.Dir = PATH
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	log.Println("running command..")
+	err := cmd.Run()
+	if err != nil {
+		log.Println("failed execute command : ", err.Error())
+		return
+	}
+	log.Println("finish command..")
 }
