d7038e176e400c6aa0d53710196883acf3d0ecb5
prima101112 <prima101112@gmail.com>
2018-09-07 10:16:59 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# WIP repo puller
+
+i need this to simplify server side projects that need to just update the workdir on server when there is push to the repo
+
+support
+- github push master
---
+++ handler.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func githubHandler(w http.ResponseWriter, r *http.Request) {
+	var res map[string]interface{}
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Println("failed to read body : ", err.Error())
+		http.Error(w, "failed to read body", 200)
+		return
+	}
+	err = json.Unmarshal(b, &res)
+	if err != nil {
+		log.Println("failed to unmarshal : ", err.Error())
+		http.Error(w, "failed to unmarshal", 200)
+		return
+	}
+
+	log.Print(res)
+}
