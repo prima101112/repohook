72f39eef846b85a3c0d5450ac321fe1e10af85c0
prima101112 <prima101112@gmail.com>
2018-09-07 13:00:42 +0700

    add post script event

--- handler.go
+++ handler.go
@@ -14,14 +14,16 @@ import (
 
 var BRANCH string
 var PATH string
+var POSTSCRIPT string
 
 func init() {
-	branchPtr := flag.String("branch", "master", "a string")
-	pathPtr := flag.String("path", "/home/go/src/github.com/repo", "a string")
+	branchPtr := flag.String("branch", "master", "branch that gonna whatch")
+	pathPtr := flag.String("path", "", "path where app will pull (default current dirrectory)")
+	postscriptPtr := flag.String("script", "", "script that will execute after pull done")
 	flag.Parse()
 	BRANCH = *branchPtr
 	PATH = *pathPtr
-
+	POSTSCRIPT = *postscriptPtr
 	_, err := exec.LookPath("git")
 	if err != nil {
 		log.Fatal("git not found")
@@ -46,7 +48,8 @@ func GithubHandler(w http.ResponseWriter, r *http.Request) {
 
 	if res["ref"] == "refs/heads/"+BRANCH {
 		pull()
-		log.Println("get push ok")
+		postevent()
+		log.Println("recheive git push : ok")
 		http.Error(w, "ok", 200)
 		return
 	}
@@ -60,6 +63,27 @@ func pull() {
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	log.Println("running command..")
+	err := cmd.Run()
+	if err != nil {
+		log.Println("failed execute command : ", err.Error())
+		return
+	}
+	log.Println("finish command..")
+}
+
+func postevent() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if POSTSCRIPT == "" {
+		POSTSCRIPT = "script/postevent.sh"
+	}
+
+	cmd := exec.CommandContext(ctx, "sh", POSTSCRIPT)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	log.Println("running command..")
+	log.Println(cmd.Args)
 	err := cmd.Run()
 	if err != nil {
 		log.Println("failed execute command : ", err.Error())
