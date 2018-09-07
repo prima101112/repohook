96a4dd17f8c08dd82bbf0966a47596f73a0b79b6
prima101112 <prima101112@gmail.com>
2018-09-07 14:05:09 +0700

    ping

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import (
 	"net/http"
 )
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "pong", 200)
+	return
+}
+
 func main() {
 	http.HandleFunc("/github", GithubHandler)
+	http.HandleFunc("/ping", ping)
 	log.Println("start server at port 9999")
 	err := http.ListenAndServe(":9999", nil)
 	if err != nil {
